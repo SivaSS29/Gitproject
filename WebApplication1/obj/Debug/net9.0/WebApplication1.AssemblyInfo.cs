a397c1c1bc5ec83d150af58f4bc59221c25584cd
SivaSS29 <sivass.ventrapragada@gmail.com>
2025-07-23 20:53:33 +0530

    latest

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18e84b52847e6ea6162924963a89ef0c8143e720")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe5075d13af734e4a74ccf290742e48e7f640f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
